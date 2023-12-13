91c488d5e8e7b7a0c8194d3223de46b12da22288
hhhmgk <ngoduchuy71123@gmail.com>
2023-12-13 15:31:06 +0700

    ...

--- data/data_process.py
+++ data/data_process.py
@@ -30,13 +30,16 @@ def no_more_dup(file, out):
     return num_dup
 
 # remove emoji, html, links
-emoji = r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])'
+# emoji = r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])'
+emoji = r'/<a?:.+?:\d{18}>|\p{Extended_Pictographic}/gu'
 html  = r'<\/?\w+((\s+\w+(\s*=\s*(?:\".*?\"|\'.*?\'|[\^\'\">\s]+))?)+\s*|\as*)\/?>'
 link = r'([(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=-]{2,256}\.[a-z]{2,6}|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\b([-a-zA-Z0-9@:%_\+~#?&//=]*)'
 def no_more_trash(file, out):
+    num_trash = 0
     with open(file,'r') as f, open(out,'w+') as f2:
         lines = f.readlines()
         for line in lines:
+            pre = len(line)
             # no emoji
             line = re.sub(emoji,'',line)
             # no html tags
@@ -44,11 +47,35 @@ def no_more_trash(file, out):
             # no links
             line = re.sub(link,'',line)
             f2.write(line)
+            if len(line) != pre:
+                num_trash += 1
+    return num_trash
 
+# remove lines with emoji, html, links
+def no_more_trash_line(file, out):
+    num_trash = 0
+    with open(file + '.lo','r') as flo, open(file + '.vi','r') as fvi, open(out + '.lo','w+') as flo2, open(out + '.vi','w+') as fvi2:
+        lines_lo = flo.readlines()
+        lines_vi = fvi.readlines()
+        n = min(len(lines_lo),len(lines_vi))
+        for i in range(0,n):
+            # if(re.search(emoji,lines_lo[i],re.IGNORECASE) != None) or (re.search(emoji,lines_vi[i],re.IGNORECASE) != None):
+            #     num_trash += 1
+            #     continue
+            if(re.search(html,lines_lo[i],re.IGNORECASE) != None) or (re.search(html,lines_vi[i],re.IGNORECASE) != None):
+                num_trash += 1
+                continue
+            if(re.search(link,lines_lo[i],re.IGNORECASE) != None) or (re.search(link,lines_vi[i],re.IGNORECASE) != None):
+                num_trash += 1
+                continue
+            flo2.write(lines_lo[i])
+            fvi2.write(lines_vi[i])
+    return num_trash
 
 # delete any case that Vi occure in La
 vietnamese = r'[áàảãạăắằẳẵặâấầẩẫậđéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵ]'
 def no_more_vi_in_lo(file, out):
+    num_vie = 0
     with open(file + '.lo','r') as flo, open(file + '.vi','r') as fvi, open(out + '.lo','w+') as flo2, open(out + '.vi','w+') as fvi2:
         lines_lo = flo.readlines()
         lines_vi = fvi.readlines()
@@ -57,11 +84,37 @@ def no_more_vi_in_lo(file, out):
             if(re.search(vietnamese,lines_lo[i],re.IGNORECASE) == None):
                 flo2.write(lines_lo[i])
                 fvi2.write(lines_vi[i])
+            else:
+                num_vie += 1
+    return num_vie
+
+# trim to only allow sen with len <= max length
+def trim(length, file, out):
+    num_trim = 0
+    with open(file + '.lo','r') as flo, open(file + '.vi','r') as fvi, open(out + '.lo','w+') as flo2, open(out + '.vi','w+') as fvi2:
+        lines_lo = flo.readlines()
+        lines_vi = fvi.readlines()
+        n = min(len(lines_lo),len(lines_vi))
+        for i in range(0,n):
+            if(len(lines_lo[i]) > length) or (len(lines_vi[i]) > length):
+                num_trim += 1
+                continue
+            flo2.write(lines_lo[i])
+            fvi2.write(lines_vi[i])
+    return num_trim
+
+def cut_first_n_lines(n, file, outTest, outTrain):
+    with open(file,'r') as f, open(outTest,'w+') as ft, open(outTrain,'w+') as ftr:
+        lines = f.readlines()
+        for i in range(0,n):
+            ft.write(lines[i])
+        for i in range(n,len(lines)):
+            ftr.write(lines[i])
 
 # create x lines version of train2023
-def create_x_lines(x):
-    file_lo = pre_processed + 'train2023.lo'
-    file_vi = pre_processed + 'train2023.vi'
+def create_x_lines(filename, x):
+    file_lo = filename + '.lo'
+    file_vi = filename + '.vi'
 
     with open(file_lo,'r') as flo, open(file_vi,'r') as fvi, open(pre_processed + f'train{x}.lo','w+') as flo2, open(pre_processed + f'train{x}.vi','w+') as fvi2:
         lines_lo = flo.readlines()
@@ -72,25 +125,6 @@ def create_x_lines(x):
             fvi2.write(lines_vi[i])
     print(f'Create {x} lines version done!')
 
-def trim(length, file, out):
-    with open(file + '.lo','r') as flo, open(file + '.vi','r') as fvi, open(out + '.lo','w+') as flo2, open(out + '.vi','w+') as fvi2:
-        lines_lo = flo.readlines()
-        lines_vi = fvi.readlines()
-        n = min(len(lines_lo),len(lines_vi))
-        for i in range(0,n):
-            if(len(lines_lo[i]) > length) or (len(lines_vi[i]) > length):
-                continue
-            flo2.write(lines_lo[i])
-            fvi2.write(lines_vi[i])
-
-def cut_first_n_lines(n, file, outTest, outTrain):
-    with open(file,'r') as f, open(outTest,'w+') as ft, open(outTrain,'w+') as ftr:
-        lines = f.readlines()
-        for i in range(0,n):
-            ft.write(lines[i])
-        for i in range(n,len(lines)):
-            ftr.write(lines[i])
-    
 raw = '/home/huy/nlp/NMT-LaVi/data/raw/'
 file_list_lo = ['dev2023.lo',
                 'train2023.lo']
@@ -102,25 +136,34 @@ file_list = file_list_lo + file_list_vi
 pre_processed = '/home/huy/nlp/NMT-LaVi/data/pre_processed/'
 print('File list: ', file_list)
 
+#DOT
 for file in file_list:
     add_dot(raw + file, pre_processed + 'adddot_' + file)
 print('Add dot done!')
 
+#DUP
 for file in file_name:
-    no_more_dup(pre_processed + 'adddot_' + file, pre_processed + 'nodup_' + file)
+    num = no_more_dup(pre_processed + 'adddot_' + file, pre_processed + 'nodup_' + file)
+    print('Removed dup in ', file, ': ', num)
 print('Remove duplicate done!')
 
-for file in file_list:
-    no_more_trash(pre_processed + 'nodup_' + file, pre_processed + 'notrash_' + file)
+#TRASH
+for file in file_name:
+    num = no_more_trash_line(pre_processed + 'nodup_' + file, pre_processed + 'notrash_' + file)
+    print('Removed trash in ', file, ': ', num)
 print('Remove trash done!')
 
+#VIETNAMESE
 for file in file_name:
-    no_more_vi_in_lo(pre_processed + 'notrash_' + file, pre_processed + 'no_vi_in_lo_' + file)
+    num = no_more_vi_in_lo(pre_processed + 'notrash_' + file, pre_processed + 'no_vi_in_lo_' + file)
     # no_more_vi_in_lo(pre_processed + 'notrash_' + file, pre_processed + file)
+    print('Removed vie in ', file, ': ', num)
 print('Remove Vi in La done!')
 
+#TRIM
 for file in file_name:
-    trim(300, pre_processed + 'no_vi_in_lo_' + file, pre_processed + file)
+    num = trim(300, pre_processed + 'no_vi_in_lo_' + file, pre_processed + file)
+    print('Trimed in ', file, ': ', num)
 print('Triming to max length done!')
 
 # cut first 1500 lines from train to make test file
@@ -137,6 +180,22 @@ for file in os.listdir(pre_processed):
         os.remove(pre_processed + file)
 print('File clean up done!')
 
-create_x_lines(1000)
-create_x_lines(10000)
-create_x_lines(50000)
+# create_x_lines(1000)
+# create_x_lines(10000)
+# create_x_lines(50000)
+# create_x_lines(raw + 'train2023', 40000)
+
+            
+
+# # create only laos, non latin sentence
+# with open(pre_processed + 'train2023.lo','r') as flo, open(pre_processed + 'train2023.vi','r') as fvi, open(pre_processed + 'train.lo','w+') as flo2, open(pre_processed + 'train.vi','w+') as fvi2:
+#     lines_lo = flo.readlines()
+#     lines_vi = fvi.readlines()
+#     n = min(len(lines_lo),len(lines_vi))
+#     for i in range(0,n):
+#         if(re.search(vietnamese,lines_lo[i],re.IGNORECASE) == None):
+#             flo2.write(lines_lo[i])
+#             fvi2.write(lines_vi[i])
+#         else:
+#             if i < 10000:
+#                 print(i,re.search(vietnamese,lines_lo[i],re.IGNORECASE),re.search(r'[a-zA-Z0-9]',lines_lo[i]))
--- data/data_visualize.py
+++ data/data_visualize.py
@@ -16,24 +16,45 @@ test = '/home/huy/nlp/NMT-LaVi/data/test/'
 
 
 # file_list = ['train2023.lo','train2023.vi']
-cnt = 1
-for file in file_list:
-    # with open(pre_processed + file,'r') as f:
-    with open(pre_processed + file,'r') as f:
-        print(file)
-        line_len = dict()
-        lines = f.readlines()
-        for line in lines:
-            if line_len.get(len(line)) == None:
-                line_len[len(line)] = 1
-            line_len[len(line)] += 1
+# file_list = ['train40000.lo','train40000.vi']
+# cnt = 1
+# for file in file_list:
+#     with open(pre_processed + file,'r') as f:
+#     # with open(raw + file,'r') as f:
+#         print(file)
+#         line_len = dict()
+#         lines = f.readlines()
+#         for line in lines:
+#             if line_len.get(len(line)) == None:
+#                 line_len[len(line)] = 1
+#             line_len[len(line)] += 1
+#             if(len(line) > 600):
+#                 print(line)
         
-        plt.subplot(2,3,cnt)
-        cnt+=1
-        keys = line_len.keys()
-        values = line_len.values()
-        plt.bar(keys, values)
-        plt.title(file)
-        # plt.hist(line_len,100)
+#         plt.subplot(1,2,cnt)
+#         cnt+=1
+#         keys = line_len.keys()
+#         values = line_len.values()
+#         plt.bar(keys, values)
+#         plt.title(file)
+#         # plt.hist(line_len,100)
+# plt.show()
 
+
+num_dup = [59, 19626] # duplicated lines
+num_trash = [51, 2318] # lines have trash (not removed)
+num_vie = [9, 165] # lines have vietnamese
+num_trim = [106, 6020] # lines have length > 300
+dev = [num_dup[0], num_trash[0], num_vie[0], num_trim[0],2000-num_dup[0]-num_trash[0]-num_vie[0]-num_trim[0]]
+train = [num_dup[1], num_trash[1], num_vie[1], num_trim[1],100000-1500-num_dup[1]-num_trash[1]-num_vie[1]-num_trim[1]]
+labels = ['Duplicate', 'Trash', 'Vietnamese', 'Length > 300','Remainings']
+plt.subplot(1,2,1)
+plt.pie(dev, labels=labels, autopct='%1.1f%%', startangle=90, counterclock=False)
+plt.title('dev2023')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.pie(train, labels=labels, autopct='%1.1f%%', startangle=90, counterclock=False)
+plt.title('train2023')
+plt.legend()
 plt.show()
