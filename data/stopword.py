7a69668f43e5cc8b70402ea2f5845b1846a79646
hhhmgk <ngoduchuy71123@gmail.com>
2023-12-15 01:56:45 +0700

    hot fix

--- data/stopword.py
+++ data/stopword.py
@@ -1,5 +1,6 @@
 import laonlp
 import re
+import os
 lao_stopwords = laonlp.lao_stopwords()
 with open('data/vietnamese-stopwords.txt','r') as f:
     vietnamese_stopwords = f.read().split('\n')
@@ -21,11 +22,46 @@ def remove_stopwords(file, stopwords, laos = False):
             words = [word for word in words if word not in stopwords]
             line = ' '.join(words)
             line = re.sub(' +', ' ', line)  # remove redundant spaces
-            f2.write(line + '\n')
+            # check if line has endline character
+            if line[-1] != '\n':
+                line += '\n'
+            f2.write(line)
     print(f'Remove stopwords from {file} done!')
 
+# for file in file_list_lo:
+#     remove_stopwords(file, lao_stopwords, True)
+# for file in file_list_vi:
+#     remove_stopwords(file, vietnamese_stopwords, False)
+
+
+def check_length(file):
+    with open(pre_processed + 'stopwords_removed/' + file,'r') as f:
+        lines = f.readlines()
+        for i in range(0,len(lines)):
+            if len(lines[i]) > 400:
+                print(file, i)
+
+# del_lines = []
+# os.rename(pre_processed + 'stopwords_removed/train2023.lo', pre_processed + 'stopwords_removed/train.lo')
+# with open(pre_processed + 'stopwords_removed/train.lo','r') as f, open(pre_processed + 'stopwords_removed/train2023.lo','w+') as f2:
+#     lines = f.readlines()
+#     for i in range(0,len(lines)):
+#         if len(lines[i]) > 400:
+#             del_lines.append(i)
+#         else:
+#             f2.write(lines[i])
+
+# os.rename(pre_processed + 'stopwords_removed/train2023.vi', pre_processed + 'stopwords_removed/train.vi')
+# with open(pre_processed + 'stopwords_removed/train.vi','r') as f, open(pre_processed + 'stopwords_removed/train2023.vi','w+') as f2:
+#     lines = f.readlines()
+#     for i in range(0,len(lines)):
+#         if i not in del_lines:
+#             f2.write(lines[i])
+
+# os.remove(pre_processed + 'stopwords_removed/train.lo')
+# os.remove(pre_processed + 'stopwords_removed/train.vi')
+                
 for file in file_list_lo:
-    remove_stopwords(file, lao_stopwords, True)
+    check_length(file)
 for file in file_list_vi:
-    remove_stopwords(file, vietnamese_stopwords, False)
-        
+    check_length(file)
